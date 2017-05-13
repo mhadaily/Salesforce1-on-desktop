b07c99d39b8e7e46a5c707a9c98054cea38b3eae
Majid <mhadaily@gmail.com>
2017-05-13 20:37:21 +0200

    add message function

--- options.js
+++ options.js
@@ -1,29 +1,30 @@
 // Saves options to chrome.storage.sync.
+function message(msg) {
+    var status = document.getElementById('status');
+    status.textContent = msg;
+    setTimeout(function() {
+        status.textContent = '';
+    }, 1500);
+}
+
 function save_options() {
     var saleforceUrl = document.getElementById('saleforceUrl').value;
+    if (!saleforceUrl) {
+        message('Error: No value specified');
+        return;
+    }
     chrome.storage.sync.set({
         saleforceUrl: saleforceUrl,
     }, function() {
         // Update status to let user know options were saved.
-        var status = document.getElementById('status');
-        status.textContent = 'Options saved.';
-        setTimeout(function() {
-            status.textContent = '';
-        }, 1500);
+        message('Options saved.');
     });
 }
 // Restores saleforceUrl using the preferences stored in chrome.storage.
 function restore_options() {
-    chrome.storage.sync.get({
-        saleforceUrl: '',
-    }, function(items) {
+    chrome.storage.sync.get('saleforceUrl', function(items) {
         document.getElementById('saleforceUrl').value = items.saleforceUrl;
     });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('saleforceUrlsave').addEventListener('click', save_options);
-
-//https://test.force.com/one/one.app
-//  if (!saleforceUrl.match(/^[a-zA-Z]+:\/\//)) {
-//         saleforceUrl = 'https://' + saleforceUrl;
-//     }
