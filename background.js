606e13601de3b940bdec97c45525388b0ce12e9c
Majid <mhadaily@gmail.com>
2017-05-13 20:37:42 +0200

    get url from storage

--- background.js
+++ background.js
@@ -1,14 +1,21 @@
 chrome.webRequest.onBeforeSendHeaders.addListener(
     function(details) {
         var url = details.url;
-        var mjReg = new RegExp(salesforceUrl, 'ig');
-        if (mjReg.test(url)) {
-            for (var i = 0; i < details.requestHeaders.length; ++i) {
-                if (details.requestHeaders[i].name === 'User-Agent') {
-                    details.requestHeaders[i].value = "Mozilla/5.0 (iPad; CPU OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1";
-                    break;
+        chrome.storage.sync.get('saleforceUrl', function(items) {
+            var mjReg = new RegExp(items.salesforceUrl, 'ig');
+            if (mjReg.test(url)) {
+                for (var i = 0; i < details.requestHeaders.length; ++i) {
+                    if (details.requestHeaders[i].name === 'User-Agent') {
+                        details.requestHeaders[i].value = "Mozilla/5.0 (iPad; CPU OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1";
+                        break;
+                    }
                 }
             }
-        }
-        return { requestHeaders: details.requestHeaders };
+            return { requestHeaders: details.requestHeaders };
+        });
     }, { urls: ["<all_urls>"] }, ["blocking", "requestHeaders"]);
+
+//https://test.force.com/one/one.app
+//  if (!saleforceUrl.match(/^[a-zA-Z]+:\/\//)) {
+//         saleforceUrl = 'https://' + saleforceUrl;
+//     }
